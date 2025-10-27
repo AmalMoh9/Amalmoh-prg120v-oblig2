0cfe1bb3828b8e03ddba2d374a97b385f764287d
AmalMoh9 <rayaanyahye@hotmail.com>
2025-10-27 18:03:49 +0100

    Remove klasse deletion functionality from PHP script

--- obligatoriskoppgave2/slett-klasse.php
+++ obligatoriskoppgave2/slett-klasse.php
@@ -1,32 +1,4 @@
-<?php
-include "db.php";
 
-// Hent liste
-$klasser = $conn->query("SELECT klassekode FROM klasse");
-?>
-
-<form method="post">
-    Velg klasse å slette:
-    <select name="klassekode">
-        <?php while ($rad = $klasser->fetch_assoc()): ?>
-            <option value="<?= $rad['klassekode'] ?>"><?= $rad['klassekode'] ?></option>
-        <?php endwhile; ?>
-    </select>
-    <input type="submit" value="Slett">
-</form>
-
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $kode = $_POST['klassekode'];
-    $slett = "DELETE FROM klasse WHERE klassekode = '$kode'";
-
-    if ($conn->query($slett)) {
-        echo "Klasse slettet.";
-    } else {
-        echo "Feil: " . $conn->error;
-    }
-}
-?>
 
 
 
