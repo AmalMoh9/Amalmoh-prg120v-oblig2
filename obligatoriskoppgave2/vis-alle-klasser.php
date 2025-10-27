a517468127abe801137946ac596f1308d0242a4e
AmalMoh9 <rayaanyahye@hotmail.com>
2025-10-27 18:03:08 +0100

    Update vis-alle-klasser.php

--- obligatoriskoppgave2/vis-alle-klasser.php
+++ obligatoriskoppgave2/vis-alle-klasser.php
@@ -1,16 +1,3 @@
-<?php
-include "db.php";
 
-$resultat = $conn->query("SELECT * FROM klasse");
-
-echo "<h2>Alle klasser</h2>";
-echo "<table border='1'><tr><th>Kode</th><th>Navn</th><th>Studium</th></tr>";
-
-while ($rad = $resultat->fetch_assoc()) {
-    echo "<tr><td>{$rad['klassekode']}</td><td>{$rad['klassenavn']}</td><td>{$rad['studiumkode']}</td></tr>";
-}
-
-echo "</table>";
-?>
 
 
