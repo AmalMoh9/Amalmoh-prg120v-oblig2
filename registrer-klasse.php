6257ab2f7785b8f7c11c2ee2379a8258f028f57b
AmalMoh9 <rayaanyahye@hotmail.com>
2025-10-27 18:35:15 +0100

    Update registrer-klasse.php

--- registrer-klasse.php
+++ registrer-klasse.php
@@ -1,25 +1,28 @@
-<?php include "db.php"; ?>
-
-<form method="post">
-    Klassekode: <input type="text" name="klassekode" required><br>
-    Klassenavn: <input type="text" name="klassenavn" required><br>
-    Studiumkode: <input type="text" name="studiumkode" required><br>
-    <input type="submit" value="Registrer">
-</form>
-
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $kode = $_POST['klassekode'];
-    $navn = $_POST['klassenavn'];
-    $studium = $_POST['studiumkode'];
+$base_url = '/app/ammoh3419-Amalmoh-prg120v-oblig2/';
+include "db_connect.php";
 
-    $sql = "INSERT INTO klasse VALUES ('$kode', '$navn', '$studium')";
-    if ($conn->query($sql)) {
-        echo "Klasse registrert!";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $klassekode = $_POST['klassekode'];
+    $klassenavn = $_POST['klassenavn'];
+    $studiumkode = $_POST['studiumkode'];
+
+    $sql = "INSERT INTO klasse (klassekode, klassenavn, studiumkode) VALUES ('$klassekode', '$klassenavn', '$studiumkode')";
+    if ($conn->query($sql) === TRUE) {
+        header("Location: ".$base_url."vis-alle-klasser.php");
+        exit();
     } else {
         echo "Feil: " . $conn->error;
     }
 }
 ?>
+<h2>Registrer ny klasse</h2>
+<form method="post" action="<?php echo $base_url; ?>registrer-klasse.php">
+    Klassekode: <input type="text" name="klassekode"><br>
+    Klassenavn: <input type="text" name="klassenavn"><br>
+    Studiumkode: <input type="text" name="studiumkode"><br>
+    <input type="submit" value="Registrer">
+</form>
+<a href="<?php echo $base_url; ?>index.php">Tilbake til meny</a>
 
 
