d1dbe76d2d04abc9e261735917aa8b3fe0c149a6
AmalMoh9 <rayaanyahye@hotmail.com>
2025-10-27 18:43:45 +0100

    Refactor database connection settings
    Updated database connection parameters with placeholders for host, username, password, and database name.

--- db_connect.php
+++ db_connect.php
@@ -1,14 +1,19 @@
 <?php
-$host = "localhost";  // Endre om Dokploy har spesifikk DB-host
-$user = "root";
-$pass = "";
-$db = "prg120v";
+
+$host = "HER_SKRIVER_DU_HOST";      // f.eks. db eller det som står i Dokploy
+$user = "HER_SKRIVER_DU_USERNAME";  // ammoh3419
+$pass = "HER_SKRIVER_DU_PASSWORD";  // 215fammoh3419
+$db   = "HER_SKRIVER_DU_DBNAME";    // ammoh3419
+
 
 $conn = new mysqli($host, $user, $pass, $db);
+
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 ?>
+
+
 
 
