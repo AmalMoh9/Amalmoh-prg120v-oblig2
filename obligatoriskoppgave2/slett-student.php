42390b487af3982e596f5a8d8d05308da6c3be16
AmalMoh9 <rayaanyahye@hotmail.com>
2025-10-27 18:03:19 +0100

    Remove student deletion functionality from PHP file

--- obligatoriskoppgave2/slett-student.php
+++ obligatoriskoppgave2/slett-student.php
@@ -1,26 +1,4 @@
-<?php
-include '../db_connect.php';
 
-if (isset($_POST['brukernavn'])) {
-  $bn = $_POST['brukernavn'];
-  $conn->query("DELETE FROM student WHERE brukernavn='$bn'");
-  echo "Student slettet.";
-}
-
-$result = $conn->query("SELECT * FROM student");
-?>
-
-<h2>Slett student</h2>
-<form method="post">
-  <select name="brukernavn">
-    <?php while ($row = $result->fetch_assoc()) { ?>
-      <option value="<?= $row['brukernavn'] ?>">
-        <?= $row['fornavn'] . " " . $row['etternavn'] ?>
-      </option>
-    <?php } ?>
-  </select>
-  <input type="submit" value="Slett">
-</form>
 
 
 
