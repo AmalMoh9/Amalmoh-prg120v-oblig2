257a2715ca596edbfee01455da205dc510270e3e
AmalMoh9 <rayaanyahye@hotmail.com>
2025-10-27 18:39:08 +0100

    Update slett-student.php

--- slett-student.php
+++ slett-student.php
@@ -1,27 +1,23 @@
 <?php
-include '../db_connect.php';
+$base_url = '/app/ammoh3419-Amalmoh-prg120v-oblig2/';
+include "db_connect.php";
 
-if (isset($_POST['brukernavn'])) {
-  $bn = $_POST['brukernavn'];
-  $conn->query("DELETE FROM student WHERE brukernavn='$bn'");
-  echo "Student slettet.";
+if(isset($_GET['klassekode'])) {
+    $kode = $_GET['klassekode'];
+    $conn->query("DELETE FROM klasse WHERE klassekode='$kode'");
+    header("Location: ".$base_url."vis-alle-klasser.php");
+    exit();
 }
 
-$result = $conn->query("SELECT * FROM student");
+$result = $conn->query("SELECT * FROM klasse");
 ?>
-
-<h2>Slett student</h2>
-<form method="post">
-  <select name="brukernavn">
-    <?php while ($row = $result->fetch_assoc()) { ?>
-      <option value="<?= $row['brukernavn'] ?>">
-        <?= $row['fornavn'] . " " . $row['etternavn'] ?>
-      </option>
-    <?php } ?>
-  </select>
-  <input type="submit" value="Slett">
-</form>
-
+<h2>Slett klasse</h2>
+<ul>
+<?php while($row = $result->fetch_assoc()) {
+    echo "<li>".$row['klassenavn']." <a href='".$base_url."slett-klasse.php?klassekode=".$row['klassekode']."'>Slett</a></li>";
+} ?>
+</ul>
+<a href="<?php echo $base_url; ?>index.php">Tilbake til meny</a>
 
 
 
